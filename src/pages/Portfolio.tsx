6997e1b986eb1ac692d9d371f230641d1cad477a
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-07-04 09:12:00 -0400

    Placeholder Portfolio page

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,11 +1,47 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+
+const projects = [
+    {
+        title: 'Gnocchia',
+        description: 'A take on the classic Snake game using just JavaScript, HTML, and CSS.',
+        imageUrl: '../assets/gnocchia.png',
+        link: 'https://gnocchia.netlify.app/'
+    },
+    {
+        title: 'Escape Plan',
+        description: 'A work-in-progress build of a web application for creating, sharing, and discovering travel itineraries.',
+        imageUrl: '../assets/escapeplan.png',
+        link: 'http://escapeplan.onrender.com/'
+    },
+    {
+        title: 'TomeTracker',
+        description: 'A companion web application for Dungeons & Dragons campaigns.',
+        imageUrl: '../assets/tometracker.png',
+        link: 'https://tome-tracker.onrender.com/'
+    }
+]
 
 const Portfolio: React.FC = () => {
     return (
         <Container className="mt-5">
-            <h1>Portfolio Page</h1>
-            <p>This is the portfolio page</p>
+            <h1 className="text-center">My Projects</h1>
+            <Row className="mt-4">
+                {projects.map((project, index) => (
+                    <Col md={4} key={index} className="mb-4">
+                        <Card>
+                            <Card.Img variant="top" src={project.imageUrl} alt={project.title}/>
+                            <Card.Body>
+                                <Card.Title>{project.title}</Card.Title>
+                                <Card.Text>{project.description}</Card.Text>
+                                <Button variant="primary" href={project.link} target="_blank">
+                                    View Project
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </Container>
     )
 }
